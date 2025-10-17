44229f572eadd5ed87e1cb22094d8497f823c6d8
Bruno José <bru.matias100@gmail.com>
2025-10-16 21:38:31 -0300

    setup

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      "quotes": ["error", "single", { "avoidEscape": true }],
+      "no-var": "error",
+      "semi": ["error", "always"],
+      "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+      "arrow-parens": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"]
+    }
+  },
+])
